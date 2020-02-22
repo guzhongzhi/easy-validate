bf63b86a4729074a1a76fab3ab522698e9e81045
guzhongzhi <zhongzhi.gu@commer.com>
2020-02-22 17:33:24 +0000

    INIT

--- src/Rules/Custom.php
+++ src/Rules/Custom.php
@@ -9,7 +9,24 @@
 namespace Guz\EasyValidate\Rules;
 
 
+/**
+ * [
+ * callback=>function($rule,$fieldName,$data,$config) {}
+ * ]
+ * Class Custom
+ * @package Guz\EasyValidate\Rules
+ */
 class Custom extends Rule
 {
-    
+    protected function _validate()
+    {
+        $callback = isset($this->config["callback"]) ? $this->config["callback"] : null;
+        $v = true;
+        if($callback) {
+            $v = call_user_func_array($callback,[$this,$this->fieldName,$this->data,$this->config]);
+        }
+        if(!$v) {
+            $this->errors[] = $this->getErrorMessage();
+        }
+    }
 }
