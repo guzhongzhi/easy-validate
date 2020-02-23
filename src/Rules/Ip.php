39762195bdb571f515d8e00147bf41107ad4ba25
guzhongzhi <zhongzhi.gu@commer.com>
2020-02-23 08:47:38 +0000

    code for this

---
+++ src/Rules/Ip.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/2/23
+ * Time: 16:46
+ */
+
+namespace Guz\EasyValidate\Rules;
+
+
+class Ip extends Rule
+{
+    const CODE = "ip";
+    
+    protected function _validate()
+    {
+        $value = $this->getValue();
+        if(filter_var($value, FILTER_VALIDATE_IP) === false) {
+            $this->generateError();
+        }
+    }
+}
